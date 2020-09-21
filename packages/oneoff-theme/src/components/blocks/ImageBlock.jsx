4c25fabeaf3c49b7174d0c7549f6e775735a4178
Stefano Mercadante <stefano.mercadante@hibo.it>
2020-09-21 15:51:19 +0200

    ImageBlock done

--- packages/oneoff-theme/src/components/blocks/ImageBlock.jsx
+++ packages/oneoff-theme/src/components/blocks/ImageBlock.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { styled } from "frontity";
-import { colors } from "../../styles";
 
 const TextBlock = ({ data, id }) => {
   const { image } = data;
 
-  return <Content></Content>;
+  return image ? <ImageStyled id={id} src={image.url}></ImageStyled> : null;
 };
 
 export default TextBlock;
 
-const Content = styled.div``;
+const ImageStyled = styled.img`
+  height: 100vh;
+  width: auto;
+`;
