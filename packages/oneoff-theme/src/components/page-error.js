3d8cec8cf075c02dfae12a44132135a864c5e46b
Stefano Mercadante <stefano.mercadante@hibo.it>
2020-09-23 11:08:27 +0200

    update error page

--- packages/oneoff-theme/src/components/page-error.js
+++ packages/oneoff-theme/src/components/page-error.js
@@ -2,34 +2,23 @@ import React from "react";
 import { styled, connect } from "frontity";
 import { colors } from "../styles";
 
-const description404 = (
-  <>
-    That page can’t be found{" "}
-    <span role="img" aria-label="confused face">
-      😕
-    </span>
-  </>
-);
-
-const description = (
-  <>
-    Don&apos;t panic! Seems like you encountered an error. If this persists,
-    <a href="https://community.frontity.org"> let us know </a> or try refreshing
-    your browser.
-  </>
-);
-
-// The 404 page component
 const Page404 = ({ state }) => {
   const data = state.source.get(state.router.link);
 
-  const title = "Oops! Something went wrong";
   const title404 = "Oops! 404";
+  const description404 = (
+    <>
+      That page can’t be found{" "}
+      <span role="img" aria-label="confused face">
+        😕
+      </span>
+    </>
+  );
 
   return (
     <Container>
-      <Title>{data.is404 ? title404 : title}</Title>
-      <Description>{data.is404 ? description404 : description}</Description>
+      <Title>{data.is404 ? title404 : ""}</Title>
+      <Description>{data.is404 ? description404 : ""}</Description>
     </Container>
   );
 };
